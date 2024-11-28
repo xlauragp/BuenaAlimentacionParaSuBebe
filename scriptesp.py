8ad7161bc06c1f284a5c5c6a242809a151cc6611
xlauragp <142696546+xlauragp@users.noreply.github.com>
2024-11-28 15:08:10 -0800

    Update scriptesp.py

--- scriptesp.py
+++ scriptesp.py
@@ -10,38 +10,38 @@ with Image.open(image_path) as img:
 
 print(f"Dimensiones de la imagen: {width}x{height}")
 
-# Define the path to the directory containing images
+# Directorios de entrada y salida
 input_folder = 'paginas'
 output_folder = 'output_images'
 
-# Create output folder if it doesn't exist
+# Crear la carpeta de salida si no existe
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-# Desired width and height for consistency
-desired_width = 1024  # Ajusta según tus necesidades
-desired_height = 1016 # Ajusta según tus necesidades
+# Desired width and height f
+desired_width = 1024  
+desired_height = 1016 
 
-# Loop through all files in the input folder
+# Procesar todas las imágenes en el directorio de entrada
 for filename in os.listdir(input_folder):
     if filename.endswith(('.png', '.jpg', '.jpeg')):
         img_path = os.path.join(input_folder, filename)
         img = Image.open(img_path)
 
-        # Apply a sharpen filter for better detail
+        # Aplicar sharpen filter para mayor detalle
         img = img.filter(ImageFilter.SHARPEN)
 
-        # Resize image maintaining aspect ratio with high quality
+        # Redimensionar la imagen
         img.thumbnail((desired_width, desired_height), Image.LANCZOS)
 
-        # Create a white background
+        # Crea un fondo blanco
         new_img = Image.new("RGB", (desired_width, desired_height), (255, 255, 255))
 
-        # Paste the resized image onto the center of the new background
+        # Pegar la imagen al centro del fondo
         offset = ((desired_width - img.size[0]) // 2, (desired_height - img.size[1]) // 2)
         new_img.paste(img, offset)
 
-        # Save the resized image to the output folder with high quality
+        #Guardar la imagen redimencionada en la carpeta de salida con mejor resolución
         output_path = os.path.join(output_folder, filename)
         new_img.save(output_path, quality=100, optimize=True)
         print(f'Resized and saved {filename} to {output_folder}')
